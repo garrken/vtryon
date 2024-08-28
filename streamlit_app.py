1a7243ea58743135ec2a79c905ed1e217195285a
garrken <106739212+garrken@users.noreply.github.com>
2024-08-28 20:42:51 +0000

    v3

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ from gradio_client import Client, handle_file
 client = Client("Kwai-Kolors/Kolors-Virtual-Try-On")
 
 # Streamlit app inställningar
-st.title("Virtuell Påklädning med Hugging Face och Gradio")
+st.title("Virtuell Try-on!")
 
 # Bilduppladdning för person och plagg
 person_img = st.file_uploader("Ladda upp Personbild", type=["png", "jpg", "jpeg"])
@@ -16,12 +16,19 @@ seed = st.slider("Välj Seed-värde", 0, 100, 0)
 randomize_seed = st.checkbox("Slumpa Seed", value=True)
 
 # När användaren klickar på knappen "Utför"
-if st.button("Utför Virtuell Påklädning"):
+if st.button("Utför Virtuell Tryon"):
     if person_img is not None and garment_img is not None:
+        # Spara uppladdade filer till tillfälliga filer
+        with open("person_img.png", "wb") as f:
+            f.write(person_img.getbuffer())
+
+        with open("garment_img.png", "wb") as f:
+            f.write(garment_img.getbuffer())
+
         # Skicka förfrågan till Hugging Face API via Gradio-klienten
         result = client.predict(
-            person_img=handle_file(person_img),
-            garment_img=handle_file(garment_img),
+            person_img=handle_file("person_img.png"),
+            garment_img=handle_file("garment_img.png"),
             seed=seed,
             randomize_seed=randomize_seed,
             api_name="/tryon"
